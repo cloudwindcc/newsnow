e005f428cb8ae9e25779813fd9ebfb2d8ddf1fc8
wangkang <wangkanghk@gmail.com>
2025-04-13 23:07:11 +1000

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">kang.ink</a>
+      <a href={`https://kang.ink/`} target="_blank">kang.ink</a>
       <span>
         <span>雲風博客 © 2024 By </span>
         <a href={Author.url} target="_blank">
