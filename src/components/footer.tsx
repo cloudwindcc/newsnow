9c2ba493299b14b120e629a86c2ff7497e2ca8be
wangkang <wangkanghk@gmail.com>
2025-04-13 23:03:00 +1000

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
+      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">kang.ink</a>
       <span>
-        <span>NewsNow © 2024 By </span>
+        <span>雲風博客 © 2024 By </span>
         <a href={Author.url} target="_blank">
           {Author.name}
         </a>
